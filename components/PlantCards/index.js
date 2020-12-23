87c4c85b5131f8d149d14a0d68f4b6f5df8d69e1
Victor Reis <victor.h.s.reis@gmail.com>
2020-12-23 09:26:21 -0300

    change to class base component

--- components/PlantCards/index.js
+++ components/PlantCards/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function PlantCards() {
+function PlantCards(props) {
   const data = {
     "data": [
       {
--- pages/Plant.js
+++ pages/Plant.js
@@ -1,59 +1,54 @@
 import Layout from '../components/Layout'
 import { Button, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react"
-import { useState } from 'react'
+import React,{Component} from 'react'
 
 const fetch = require('node-fetch')
 import PlantCards from '../components/PlantCards';
-export default function About(){
+class About extends React.Component{
+  constructor(){
+    super(props)
+    this.state = {plant:""}
+    this.handlerInputChange = this.handlerInputChange.bind(this)
+  }
 
-  const [plant,setPlant] = useState("")
-
-  const handlerInputChange = async (event) =>{
+    handlerInputChange = async (event) =>{
     setPlant(event.target.value)
-    
     
     if (event.target.value.length+1 >= 3){
       const response = await fetch('http://localhost:3000/api/hello?plant='+event.target.value.length);
-      const json = await response.json();
-      console.log(json);
+       this.state.plant = await response.json();
+      for (let i in plants.data){
+        console.log(plants.data[i].id)
+      }
       
     }
   }
-  return(
-      <Layout>
-        <h1>Find a plant</h1>
-        <p>Insert a plant, specie or popular name</p>
-        <InputGroup size='md'>
-          <Input
-          pr='4.5rem'
-          type='text'
-          placeholder='Insert here'
-          onChange={handlerInputChange}
-          />
-          <InputRightElement width='4.5rem'>
-            <Button type='submit' h='1.75rem' size='sm' >
-              enter
-            </Button>
-          </InputRightElement>
-        </InputGroup>
+  render(){
+    return(
+        <Layout>
+          <h1>Find a plant</h1>
+          <p>Insert a plant, specie or popular name</p>
+          <InputGroup size='md'>
+            <Input
+            pr='4.5rem'
+            type='text'
+            placeholder='Insert here'
+             onChangeCapture={handlerInputChange}
+            />
+            <InputRightElement width='4.5rem'>
+              <button type='submit' h='1.75rem' size='sm'  >
+                enter
+              </button>
+            </InputRightElement>
+          </InputGroup>
+    
+          <hr/>
+          <div className="ContainerCard">
+          </div>
+        </Layout>
+    )
 
-        <hr/>
-        <div className="ContainerCard">
-          <PlantCards/>
-          <PlantCards/>
-          <PlantCards/>
-          <PlantCards/>
-  
-          <PlantCards/>
-          <PlantCards/>
-          <PlantCards/>
-          <PlantCards/>
-
-          <PlantCards/>
-          <PlantCards/>
-          <PlantCards/>
-          <PlantCards/>
-        </div>
-      </Layout>
-  )
+  }
+    
 }
+export default About;
