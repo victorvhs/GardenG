108f45800439166840c79b34d329069b13cc824b
Victor Reis <victor.h.s.reis@gmail.com>
2020-12-23 12:06:26 -0300

    logica de render

--- pages/Plant.js
+++ pages/Plant.js
@@ -1,49 +1,62 @@
 import Layout from '../components/Layout'
-import { Button, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react"
+//import { Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react"
 import React,{Component} from 'react'
-
 const fetch = require('node-fetch')
 import PlantCards from '../components/PlantCards';
+import data from './data'
 class About extends React.Component{
   constructor(){
-    super(props)
-    this.state = {plant:""}
-    this.handlerInputChange = this.handlerInputChange.bind(this)
-  }
-
-    handlerInputChange = async (event) =>{
-    setPlant(event.target.value)
-    
-    if (event.target.value.length+1 >= 3){
-      const response = await fetch('http://localhost:3000/api/hello?plant='+event.target.value.length);
-       this.state.plant = await response.json();
-      for (let i in plants.data){
-        console.log(plants.data[i].id)
-      }
-      
+    super()
+    this.state = {
+      plant:"",
+      respPlant:"",
+      obj: "",
     }
+    this.handlerInputChange = this.handlerInputChange.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  
+  async handlerInputChange (event) {
+      event.preventDefault()
+      let elm = []
+      data.forEach((item)=>{
+        elm.push( <PlantCards data={item}/>)
+      })
+      this.setState({obj:elm})
+
+      return
+  }
+  
+  handleChange(event){
+    const {name,value} = event.target
+    this.setState({[name]:value})
+  }
+  handleClick(){
+    this.setState({obj:''})
+    console.log('clear')
   }
   render(){
+ 
     return(
         <Layout>
           <h1>Find a plant</h1>
           <p>Insert a plant, specie or popular name</p>
-          <InputGroup size='md'>
-            <Input
-            pr='4.5rem'
-            type='text'
-            placeholder='Insert here'
-             onChangeCapture={handlerInputChange}
-            />
-            <InputRightElement width='4.5rem'>
-              <button type='submit' h='1.75rem' size='sm'  >
-                enter
-              </button>
-            </InputRightElement>
-          </InputGroup>
-    
-          <hr/>
+          <form onSubmit={this.handlerInputChange}>
+              <input
+              name='plant'
+              type='text'
+              placeholder='Insert here'
+              value={this.state.plant}
+              onChange={this.handleChange}
+              />
+                <button type='submit' h='1.75rem' size='sm'  >
+                  enter
+                </button>
+          </form>
+                <button onClick={this.handleClick}>Clear</button>
           <div className="ContainerCard">
+          {this.state.obj}
           </div>
         </Layout>
     )
