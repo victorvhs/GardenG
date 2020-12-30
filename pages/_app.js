21f94f2676e907ffb4e48ae53f3a0c9457663735
Victor Reis <victor.h.s.reis@gmail.com>
2020-12-29 23:15:47 -0300

    import css

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,7 @@
 import '../styles/globals.css'
 import '../styles/Card.css'
 import '../styles/PlantsCards.css'
-import 'tailwindcss/tailwind.css'
-import {ChakraProvider} from "@chakra-ui/react"
+
 function MyApp({ Component, pageProps }) {
   return(
   <div>
